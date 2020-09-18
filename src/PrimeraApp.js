190e70458b6168fa2ad37ccbd313b5f363a1aa66
JUAN VICTOR SERRUDO CHAVEZ <juan.vsc@gmail.com>
2020-09-17 23:59:08 -0400

    20200917 Avances hasta la clase 41

---
+++ src/PrimeraApp.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// const PrimeraApp = ({ saludo = 'Hola mundo' }) => {
+// const PrimeraApp = ({ saludo }) => {
+const PrimeraApp = ({ saludo, subtitulo }) => {
+
+    // const saludo = {
+    //     nombre: 'Juan',
+    //     edad: 33
+    // };
+
+    // const saludo = 'Hola Mundo';
+
+    // console.log(props);
+
+    // if ( ! saludo ) {
+    //     throw new Error( 'El saludo es necesario' );
+    // }
+
+    return (
+        <>
+            <h1> { saludo } </h1>
+            {/* <pre> { JSON.stringify(saludo, null, 3) } </pre> */}
+            <p> { subtitulo } </p>
+        </>
+    );
+
+}
+
+
+// === VALIDACION DE COMPONENTES ===
+PrimeraApp.propTypes = {
+    // saludo: PropTypes.string // NO OBLIGATORIO
+    saludo: PropTypes.string.isRequired
+}
+
+
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un subtitulo'
+}
+
+
+export default PrimeraApp;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const divRoot = document.querySelector('#root');
+
+
+// ReactDOM.render( <PrimeraApp saludo="Hola, soy Goku" />, divRoot );
+
+// ReactDOM.render( <PrimeraApp saludo={123} />, divRoot ); // Envio de valor númerico
+
+// ReactDOM.render( <PrimeraApp saludo="Hola mundo" />, divRoot );
+
+ReactDOM.render( <CounterApp value={123} />, divRoot );
